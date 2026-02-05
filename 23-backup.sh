7ef9553b3e2e7498475be4c0b955d974822a3f14
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-02-05 21:39:37 +0530

    delete logs

--- 23-backup.sh
+++ 23-backup.sh
@@ -12,6 +12,10 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} # 14 days is the default value, if the user not supplied
 
+log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
+
 if [ $USERID -ne 0 ]; then
     echo -e $R "Please run this command with sudo access only" $N 
     exit 1
@@ -36,4 +40,38 @@ fi
 if [ ! -d $DEST_DIR_DIR ]; then
     echo "$R $DEST_DIR does not exist $N"
     exit 1
+fi
+
+### Find the files
+FILES=$(find "$SOURCE_DIR" -name "*.log" -type f -mtime +$DAYS)
+
+log "Backup started"
+log "Source Directory: $SOURCE_DIR"
+log "Destination Directory: $DEST_DIR"
+log "Days: $DAYS"
+
+if [ -z "${FILES}" ]; then
+    log "No files to archieve ... $Y Skipping $N"
+else
+    # app-logs-$timestamp.zip
+    log "Files found to archieve: $FILES"
+    TIMESTAMP=$(date +%F-%H-%M-%S)
+    ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.tar.gz"
+    log "Archieve name: $ZIP_FILE_NAME"
+    tar -zcvf $ZIP_FILE_NAME $(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+
+    # Check archieve is success or not
+    if [ -f $ZIP_FILE_NAME ]; then
+        log "Archeival is ... $G SUCCESS $N"
+
+        while IFS= read -r filepath; do
+        # Process each line here
+        log "Deleting file: $filepath"
+        rm -f $filepath
+        log "Deleted file: $filepath"
+        done <<< $FILES
+    else
+        log "Archeival is ... $R FAILURE $N"
+        exit 1
+    fi
 fi
