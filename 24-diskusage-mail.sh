2b33685783faa55a7645cc117c451af92b192bfe
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-02-06 22:05:23 +0530

    delete logs

--- 24-diskusage-mail.sh
+++ 24-diskusage-mail.sh
@@ -30,5 +30,4 @@ done <<< $DISK_USAGE
 
 echo -e $MESSAGE
 
-sh mail.sh "mskumarmadhavarapu@gmail.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" 
-"$IP_ADDRESS" "DevOps Team"
+sh mail.sh "mskumarmadhavarapu@gmail.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
